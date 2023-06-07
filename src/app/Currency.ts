d260bf208936dce09ce967fb4a7fc4f4137030b3
Abanoub5015 <abanoub5016@gmail.com>
2023-06-07 07:35:19 +0300

    one of the greatest help from Jesus  currency-convert-btn updated

--- src/app/Currency.ts
+++ src/app/Currency.ts
@@ -1,14 +1,8 @@
 
 export class Currency{
   rates: any;
-  full_name: string;
-  name: string;
-  symbol: string;
-
+  
   constructor(){
       this.rates = [];
-      this.full_name= '';
-      this.name= '';
-      this.symbol= '';
   }
 }
