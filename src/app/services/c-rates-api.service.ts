d72f0c3136400c5e245229b7b086846673fc9fe7
Abanoub5015 <abanoub5016@gmail.com>
2023-06-07 07:31:01 +0300

    " one of the greatest help from Jesus "
    currency-convert-btn updated ✔

--- src/app/services/c-rates-api.service.ts
+++ src/app/services/c-rates-api.service.ts
@@ -12,20 +12,20 @@ export class CRatesApiService {
 
   getCurrencyRates(): Observable<Currency[]>  // (<>) means (type assertion)
   {
-    let url = 'http://data.fixer.io/api/latest?access_key=754c0df0b3de0b8bfa23d8f062c5756c';
+    let url = 'http://data.fixer.io/api/latest?access_key=c0021af12c63d3620656e68df666b5f7';
     return this.http.get<Currency[]>(url);
   }
 
 /*   getCurrencyRates()
   {
-    let url = 'http://data.fixer.io/api/latest?access_key=754c0df0b3de0b8bfa23d8f062c5756c';
+    let url = 'http://data.fixer.io/api/latest?access_key=c0021af12c63d3620656e68df666b5f7';
     return this.http.get(url);
   } */
 
   getCurrencyhistorical()
   {
     let date = ['2022-05-31','2022-06-30','2022-07-31','2022-08-31','2022-09-30','2022-10-31','2022-11-30','2022-12-31','2023-01-31','2023-02-28','2023-03-31','2023-04-30'];
-    let url = 'http://data.fixer.io/api/' + 'date' + '?access_key=754c0df0b3de0b8bfa23d8f062c5756c';
+    let url = 'http://data.fixer.io/api/' + 'date' + '?access_key=c0021af12c63d3620656e68df666b5f7';
     let chartList = [];
     chartList.push(url)
    // console.log(chartList);
